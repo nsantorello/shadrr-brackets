998a0de4e366f811e27a080534e5c22d8270264c
Noah Santorello <nsantorello@gmail.com>
2015-03-05 01:37:58 -0500

    Remove clients that can't be reached

--- devices.js
+++ devices.js
@@ -12,30 +12,40 @@ define(function (require, exports, module) {
         return data.host + ":" + data.port;   
     }
     
+    function addClient(client) {
+        Devices.clients.push(client);
+        console.log("[Shadrr/devices.js] Added client:", client);        
+    }
+    
+    function removeClient(client) {
+        _.remove(Devices.clients, function(c) { return c == client; });
+        console.log("[Shadrr/devices.js] Removed client:", client);
+    }
+    
     mdns.shadrrUp(function(data) {
         var client = getClient(data);
         if (!_.contains(Devices.clients, client)) {
-            Devices.clients.push(client);
-            console.log("[Shadrr/devices.js] Added client:", client);
+            addClient(client);
         }
     });
     
     mdns.shadrrDown(function(data) {
         var client = getClient(data);
         if (_.contains(Devices.clients, client)) {
-            _.remove(Devices.clients, function(c) { return c == client; });
-            console.log("[Shadrr/devices.js] Removed client:", client);
+            removeClient(client);
         }
     });
     
     Devices._broadcastToDevice = function(client, file, code) {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("POST", "http://" + client, true);
-        xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlHttp.send("file=" + file + "&code=" + code);
-        
-        // TODO: handle HTTP errors and potentially remove the device or flag it as bad
-        // TODO: need to url escape the code probably
+        try {
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.open("POST", "http://" + client, true);
+            xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xmlHttp.send("file=" + file + "&code=" + code);
+        } catch (ex) {
+            console.log("[Shadrr/devices.js] Removing stale client '" + client + "'");
+            removeClient(client);
+        }
     }
     
     Devices.broadcast = function(file, code) {
